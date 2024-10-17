5f1f61cc3db385c9237506713e911b6ac8522199
Rubén García Recio <rgarciar19042002@gmail.com>
2024-10-17 10:42:36 +0200

    Add files via upload

---
+++ Rayuela/Rayuela/ClaseFaltas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rayuela
+{
+    class ClaseFaltas
+    {
+        private int Indice;
+        private String IdentificadorAlumno;
+        private String Fecha;
+        private String Modulo;
+
+        public ClaseFaltas()
+        {
+        }
+
+        public ClaseFaltas(int indice, string identificadorAlumno, string fecha, string modulo)
+        {
+            Indice = indice;
+            IdentificadorAlumno = identificadorAlumno;
+            Fecha = fecha;
+            Modulo = modulo;
+        }
+
+        public int Indice1 { get => Indice; set => Indice = value; }
+        public string IdentificadorAlumno1 { get => IdentificadorAlumno; set => IdentificadorAlumno = value; }
+        public string Fecha1 { get => Fecha; set => Fecha = value; }
+        public string Modulo1 { get => Modulo; set => Modulo = value; }
+    }
+}
---
+++ Rayuela/Rayuela/Ficha.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rayuela
+{
+    public partial class Ficha : Form
+    {
+        ClaseAlumno alumno=new ClaseAlumno();
+        List<ClaseModulos> listaModulos= new List<ClaseModulos>(); 
+        public Ficha()
+        {
+            InitializeComponent();
+        }
+
+        internal ClaseAlumno Alumno { get => alumno; set => alumno = value; }
+
+        private void Ficha_Load(object sender, EventArgs e)
+        {
+            ClaseConectar cnx=new ClaseConectar(); 
+            this.Text = alumno.Nombre;
+            lblId.Text=alumno.Identificador.ToString();
+            lblNombre.Text=alumno.Nombre;
+            lblNum.Text = alumno.Curso.ToString();
+            lblCurso.Text = alumno.Ciclo;
+            lblCorreo.Text = alumno.Mail.ToString();
+            pctImagen.Image=Image.FromFile(alumno.Imagen);
+            listaModulos = cnx.listarModulos(alumno.Ciclo,alumno.Curso);
+            foreach(ClaseModulos cm in listaModulos)
+            {
+                cmbFaltas.Items.Add(cm.NombreCompleto.ToString());
+
+            }
+            
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInformar_Click(object sender, EventArgs e)
+        {
+            FormInformes form = new FormInformes();
+            form.ShowDialog();
+        }
+
+        private void btnAsistencia_Click(object sender, EventArgs e)
+        {
+            if (cmbFaltas.Text.Length > 0)
+            {
+                ClaseConectar cnx = new ClaseConectar();
+                cnx.insertarFalta(alumno.Identificador, cmbFaltas.Text);
+            }
+            else
+            {
+                MessageBox.Show("Debes seleccionar algún módulo");
+            }
+            
+        }
+    }
+}
